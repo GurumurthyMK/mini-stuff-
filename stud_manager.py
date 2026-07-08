49ddfc13b4eac2b084fc85e8926929a79d41fa86
grootonius <gurumurthymk2@gmail.com>
2026-07-08 11:26:07 +0530

    added mini project

--- stud_manager.py
+++ stud_manager.py
@@ -6,6 +6,7 @@
 # Find average
 # Count pass/fail
 # Search student by name
+# Display all
 
 # Initializing student DB in a list of dicts 
 stud_db: list = [
@@ -18,16 +19,16 @@ stud_db: list = [
 
 ]
 
-
-# Definig CLI menu 
-def opr_menu(DB:list):
+def operations(DB:list):
     
-    # Defining logic for each feature 
+    # defining logic for each feature 
+    
+    # addin student
     def add_stud():
         name = input("Enter the name : ")
         course = input("Enter the course : ")
         roll = input("Enter roll : ")
-        final_percentage = input("Enter final percentage : ")
+        final_percentage = int(input("Enter final percentage : "))
 
         DB.append({
             "Name": name,
@@ -35,7 +36,9 @@ def opr_menu(DB:list):
             "Roll": roll,
             "Final percentage": final_percentage
         })
+        print("Added successfully\n")
 
+    # removing student
     def remove_stud():
         name = input("Enter student name to be removed: ")
 
@@ -45,25 +48,66 @@ def opr_menu(DB:list):
                 break 
         if index is not None:
             del DB[i]
-            print("Deleted entry successfully\n", DB)
+            print("Deleted entry successfully\n")
         else:
             return "record not found"
+    
+    # updating centage
+    def update_marks():
+        name = input("Enter the name of student who's marks need to be updated: ")
         
+        for item in DB:
+            if item["Name"] == name:
+                print("Data found, current final percentage is: ", {item["Final percentage"]}) 
+                item["Final_percentage"] = input("Enter the updated percentage: ")
+                print("Updated succesfully\n")
+                break 
+        else:
+            return "Invalid input" 
+
+
+    # Find topper 
+    def find_topper():
+        topper = max(DB, key=lambda stud: stud["Final percentage"])
+        if topper in DB:
+            print(f"Topper: {topper["Name"]} \n Percentage: {topper["Final percentage"]}")
+        return "Enter a valid input"
+    
+    # find average
+    def find_avg():
+        for stud in DB:
+            avg = sum(stud["Final percentage"] for stud in DB) / len(DB)
+            print(f"The total average is: {avg}")
+        return "Enter a valid input"
+
+    # counting pass and fail
+    def p_f():
+        n = 35
+        Pass = sum( 1 for stud in DB if stud["Final percentage"] > n )
+        fail = sum( 1 for stud in DB if stud["Final percentage"] < n )
+        print(f"Number of passed students: {Pass} \n Number of failed students: {fail}")    
+   
+    # find by name
+    def find_name():
+        name = input("Enter student name to be found: ")
+        for stud in DB:
+            if stud["Name"] == name:
+                print(f"Data found \nName: {stud["Name"]}\n"
+                        f"\nCourse: {stud["Course"]}"
+                        f"\nRoll: {stud["Roll"]}"
+                        f"\nFinal percent: {stud["Final percentage"]}")
+            return "Not found"
+    
+    # displaying students
     def display_studs():
         for item in DB:
             print(item)
+     
+     
 
-         
-  # routing requests     
-    if  "Add" and "add" in opr:
-        add_stud()
-    if  "Remove" and "remove" in opr:
-        remove_stud()
-    if  "Display" and "display" in opr:
-        add_stud()
-
+    # definig CLI menu 
     while True:
-        opr = (input("Choose an operation from below: "
+        opr = (input("\n Choose an operation from below: "
                      "\nAdd student"
                      "\nRemove student"
                      "\nUpdate marks"
@@ -72,10 +116,30 @@ def opr_menu(DB:list):
                      "\nCount pass/fail"
                      "\nSearch student by name"
                      "\nDisplay students"
+                     "\nExit"
                      "\n"
-                ))
+                )).lower()
+    
+        # routing requests     
+        if "add" in opr:
+            add_stud()
+        elif "remove" in opr:
+            remove_stud()
+        elif "update" in opr:
+            update_marks()
+        elif "find topper" in opr:
+            find_topper()
+        elif "find average" in opr:
+            find_avg()
+        elif "count" in opr:
+            p_f()
+        elif "search" in opr:
+            find_name()
+        elif "display" in opr:
+            display_studs()
+        elif "exit" in opr:
+            break
+        else:
+            return "Invalid option" 
 
-
-# 4. Updating and sorting DB after operations 
-
-opr_menu(DB=stud_db)
+operations(DB=stud_db)
